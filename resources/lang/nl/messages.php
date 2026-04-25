a2aa2aacca85c7ad33ad43249e1e5bd19b7f7829
Cordina90 <pierbraaksma@hotmail.com>
2026-04-25 15:10:32 +0200

    Added Dutch localization

---
+++ resources/lang/nl/messages.php
@@ -0,0 +1,83 @@
+<?php
+
+return [
+    // Navigation
+    'navigation_group' => 'Berichten',
+    'inbox' => 'Inbox',
+    'sent' => 'Verzonden',
+    'starred' => 'Gemarkeerd',
+    'trash' => 'Prullenbak',
+
+    // Table columns
+    'from' => 'Van',
+    'to' => 'Aan',
+    'subject' => 'Onderwerp',
+    'received' => 'Ontvangen',
+    'sent_at' => 'verzonden op',
+    'starred_at' => 'Gemarkeerd op',
+    'deleted_at' => 'Verwijderd op',
+    'status' => 'Status',
+
+    // Read status
+    'unread' => 'Ongelezen',
+    'read' => 'Gelezen',
+    'read_count' => 'Read :read/:total',
+
+    // Filters
+    'unread_only' => 'Alleen ongelezen',
+    'starred_only' => 'Alleen gemarkeerd',
+
+    // Actions
+    'compose' => 'Opstellen',
+    'compose_message' => 'Bericht opstellen',
+    'reply' => 'Beantwoorden',
+    'reply_all' => 'Alles beantwoorden',
+    'forward' => 'Doorsturen',
+    'forward_message' => 'Bericht doorsturen',
+    'star' => 'Markeer',
+    'unstar' => 'De-markeer',
+    'mark_as_read' => 'Markeer als gelezen',
+    'move_to_trash' => 'Verplaats naar prullenbak',
+    'restore' => 'Herstellen',
+    'delete_permanently' => 'Definitief verwijderen',
+    'empty_trash' => 'Prullenbak legen',
+    'back_to_inbox' => 'Terug naar inbox',
+    'back_to_sent' => 'Terug naar verzonden',
+    'remove_from_sent' => 'Verwijderen uit verzonden',
+    'delete' => 'Verwijderen',
+
+    // Compose form
+    'recipient_to' => 'Aan',
+
+    // Notifications
+    'message_sent' => 'Bericht verzonden',
+    'reply_sent' => 'Antwoord verzonden',
+    'message_forwarded' => 'Bericht doorgestuurd',
+    'message_trashed' => 'Bericht verplaatst naar prullenbak',
+    'message_removed_from_sent' => 'Bericht verwijderd uit verzonden',
+    'message_restored' => 'Bericht hersteld',
+    'message_deleted' => 'Bericht permanent verwijderd',
+    'trash_emptied' => 'Prullenbak geleegd',
+    'new_message_from' => 'Nieuw bericht van :name',
+    'replied' => ':name heeft geantwoord: :subject',
+
+    // Forward
+    'forwarded_message_header' => '---------- Doorgestuurd bericht ----------',
+    'forwarded_from' => 'Van: :name',
+    'forwarded_date' => 'Datum: :date',
+    'forwarded_subject' => 'Onderwerp: :subject',
+    'forwarded_to' => 'Aan: :names',
+
+    // Confirmations
+    'confirm_trash' => 'Weet je zeker dat je dit bericht naar de prullenbak wilt verplaatsen?',
+    'confirm_delete' => 'Weet je zeker dat je dit bericht permanent wilt verwijderen?',
+    'confirm_empty_trash' => 'Weet je zeker dat je alle berichten in de prullenbak permanent wilt verwijderen?',
+
+    // Thread view
+    'you' => 'Jij',
+
+    // Stats widget
+    'unread_messages' => 'Ongelezen berichten',
+    'total_messages' => 'Totaal berichten',
+
+];
